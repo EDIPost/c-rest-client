cd88cca956d5228a8e9304728869bf1d5546547c
Ola <ola@quick.no>
2013-07-08 02:29:00 +0200

    Initial commit

---
+++ EDIPostService/EDIPostServiceTests/ServiceConnection/ServiceConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using EPService = EDIPostService.ServiceConnection;
+
+namespace EDIPostServiceTests.ServiceConnection
+{
+    public class ServiceConnection
+    {
+        [Fact]
+        public void ConstructorTest()
+        {
+            EPService.ServiceConnection sc = new EPService.ServiceConnection("http://api.edipost.no", "12312-1231321-12312312");
+
+            Assert.Equal("http://api.edipost.no", sc.baseurl);
+        }
+    }
+}
---
+++ EDIPostService/ServiceConnection/ServiceConnection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDIPostService.ServiceConnection
+{
+    public class ServiceConnection
+    {
+
+        /// <summary>
+        /// Holds the URL for EDIPost API interface
+        /// </summary>
+        private string _baseurl;
+        public string baseurl
+        {
+            get
+            {
+                return this._baseurl;
+            }
+            set
+            {
+                this._baseurl = value;
+            }
+        }
+
+        /// <summary>
+        /// Holds the API key to identify the user
+        /// </summary>
+        private string _apikey;
+        public string apikey
+        {
+            get
+            {
+                return this._apikey;
+            }
+            set
+            {
+                this._apikey = value;
+            }
+        }
+        
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="base_url">EDIPost API url</param>
+        /// <param name="api_key">EDIPost API key</param>
+        public ServiceConnection(string base_url, string api_key)
+        {
+            baseurl = base_url;
+            apikey = api_key;
+        }
+
+       
+    }
+}
